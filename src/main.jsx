40c76ff9bd235923dfe55982398f5ae6195061d3
Deem3 <erdemee.t@gmail.com>
2023-01-31 00:37:15 +0800

    modified:   package-lock.json 	modified:   package.json 	new file:   public/index.html 	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 // contexts
@@ -9,9 +9,9 @@ import { LoadingProvider } from "./utils/context/LoadingContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LoadingProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
     </LoadingProvider>
   </React.StrictMode>
 );
